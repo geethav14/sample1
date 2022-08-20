56363ac449f8188be6bfa3c943477fe0b276bbdb
geethav14 <geethaunique14@gmail.com>
2022-08-20 10:00:37 +0530

    new code

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -2,7 +2,7 @@ package org.computer;
 
 public class Computer {
 public void comModel() {
-	System.out.println("Computer Model - Dell 14 Vostro 3400");
+	System.out.println("Computer Model - Dell 11 Vostro 3400");
 }
 public static void main(String[] args) {
 	Computer c = new Computer();
